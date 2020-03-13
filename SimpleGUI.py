6c8e08a5399883279849a10541ec6d6cef349eb7
FlynnEastwood <aflynneastwood@gmail.com>
2020-03-12 23:32:00 -0400

    Update SimpleGUI.py
    Simplify to a button.

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -1,35 +1,23 @@
 from tkinter import *
 
-from tkinter.ttk import *
-
-from tkinter import messagebox
-
+#Fenetres
 window = Tk()
 
-window.title("Welcome to LikeGeeks app")
+#Body
+window.title("LazyPost Minds.com")
+window.geometry('350x300')
 
-selected = IntVar()
 
-rad1 = Radiobutton(window,text='First', value=1, variable=selected)
+lbl = Label(window, text="Send to Minds.com")
 
-rad2 = Radiobutton(window,text='Second', value=2, variable=selected)
-
-rad3 = Radiobutton(window,text='Third', value=3, variable=selected)
+lbl.grid(column=0, row=0)
 
 def clicked():
-    messagebox.showinfo('Info', 'You have selected ' + str(selected.get()))
 
+    lbl.configure(text="Button was clicked !!")
 
-    print(selected.get())
+btn = Button(window, text="Go!", command=clicked)
 
-btn = Button(window, text="Click Me", command=clicked)
-
-rad1.grid(column=0, row=0)
-
-rad2.grid(column=1, row=0)
-
-rad3.grid(column=2, row=0)
-
-btn.grid(column=3, row=0)
+btn.grid(column=1, row=1)
 
 window.mainloop()

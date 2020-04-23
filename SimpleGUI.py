af9410a97b6d45c7079594a60721a58401148734
FlynnEastwood <aflynneastwood@gmail.com>
2020-04-22 23:20:30 -0400

    Added buttons to GUI
    Need to fire functions from buttons

--- SimpleGUI.py
+++ SimpleGUI.py
@@ -1,23 +1,38 @@
 from tkinter import *
 
 #Fenetres
-window = Tk()
+def spawnWindow():
+	window = Tk()
 
-#Body
-window.title("LazyPost Minds.com")
-window.geometry('350x300')
+	#Body
+	window.title("LazyPost Minds.com")
+	window.geometry('400x500')
 
 
-lbl = Label(window, text="Send to Minds.com")
+	lbl = Label(window, text="Platform", height=2, font=("Helvetica", 16))
 
-lbl.grid(column=0, row=0)
+	lbl.grid(column=2, row=0)
 
-def clicked():
+	def clicked():
 
-    lbl.configure(text="Button was clicked !!")
+	    newLbl = Label(window, text="Go!", height=2, font=("Helvetica", 16))
+	    newLbl.grid(column=2, row=2)
+		
 
-btn = Button(window, text="Go!", command=clicked)
+	btn01 = Button(window, text="Minds",  width=10, height=3, command=clicked)
+	btn02 = Button(window, text="Steemit", width=10, height=3, command=clicked)
+	btn03 = Button(window, text="Bitchute", width=10, height=3, command=clicked)
+	btn04 = Button(window, text="LBRY", width=10, height=3, command=clicked)
+	btn05 = Button(window, text="D.Tube", width=10, height=3, command=clicked)
 
-btn.grid(column=1, row=1)
+	btn01.grid(column=0, row=1)
+	btn02.grid(column=1, row=1)
+	btn03.grid(column=2, row=1)
+	btn04.grid(column=3, row=1)
+	btn05.grid(column=4, row=1)
 
-window.mainloop()
+
+	window.mainloop()
+
+if __name__== "__main__":
+  spawnWindow()

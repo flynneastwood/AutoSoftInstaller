c70ad41bb4406e198c9188d67e2ee79f0b3ffee4
FlynnEastwood <aflynneastwood@gmail.com>
2020-04-23 17:08:35 -0400

    GUI now works!
    Buttons works but window unresponsive.

--- SimpleSelenium.py
+++ SimpleSelenium.py
@@ -1,5 +1,6 @@
 import json
 
+from tkinter import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -34,7 +35,6 @@ videoMediaList.append(str(videoPath))
 thumbnail = './thumbnail.jpg'
 
 def main():
-
 
     def minds():
 
@@ -285,11 +285,58 @@ def main():
     def youtube():
         pass
 
+    def gui():
+
+        window = Tk()
+
+        #Body
+        window.title("LazyPost Minds.com")
+        window.geometry('400x500')
 
 
-    bitchute()
+        lbl = Label(window, text="Send to Minds.com")
+        lbl = Label(window, text="Platform", height=2, font=("Helvetica", 16))
+
+        lbl.grid(column=0, row=0)
+        lbl.grid(column=2, row=0)
+
+        def clickedMinds():
+
+            minds()
+
+        def clickedSteemit():
+
+            steemit()
+
+        def clickedBitchute():
+
+            bitchute()
+
+        def clickedLBRY():
+
+            lbry()
+
+        def clickedDtube():
+
+            dTube()
+
+
+        btn01 = Button(window, text="Minds",  width=10, height=3, command=clickedMinds)
+        btn02 = Button(window, text="Steemit", width=10, height=3, command=clickedSteemit)
+        btn03 = Button(window, text="Bitchute", width=10, height=3, command=clickedBitchute)
+        btn04 = Button(window, text="LBRY", width=10, height=3, command=clickedLBRY)
+        btn05 = Button(window, text="D.Tube", width=10, height=3, command=clickedDtube)
+
+
+        btn01.grid(column=0, row=1)
+        btn02.grid(column=1, row=1)
+        btn03.grid(column=2, row=1)
+        btn04.grid(column=3, row=1)
+        btn05.grid(column=4, row=1)
+
+        window.mainloop()
     
-
+    gui()
     #driver.close()
 if __name__== "__main__":
   main()

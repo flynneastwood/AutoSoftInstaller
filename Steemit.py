42e115a15d79dc2220c134ad25dabfa1d62042c6
flynneastwood <aflynneastwood@gmail.com>
2020-11-19 06:06:17 -0500

    Added more post content to .json and steemit basically works. Can't send images though.

--- Steemit.py
+++ Steemit.py
@@ -1,5 +1,4 @@
 import json
-import pyautogui
 import os
 
 from selenium import webdriver
@@ -10,69 +9,72 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-from ParsePost import getTitle, getBody, getHashtags, getVideoDesc
-
-
 driver = webdriver.Chrome('./chromedriver')
 
 with open('ids.json', 'r') as json_file:     #get credentials from json
-    data = json.load(json_file)
+	data = json.load(json_file)
 with open('postContent.json', 'r') as json_file:     #get post content
-    content = json.load(json_file)
+	content = json.load(json_file)
 
-title = (content['steemit']['title'])
+title = (content['GENERAL']['title'])
+body = (content['GENERAL']['body'])
+steemitBody = (content['steemit']['body'])
 tags = (content['steemit']['tags'])
-body = (content['steemit']['body'])
+
 
 videoUrl = 'https://www.youtube.com/'
 
-thumbnail = './thumbnail.jpg'
+thumbnail = '/home/tony/LazyPost/content/16_Forest.jpg'
 
 def steemit():
 
-        driver.get("https://www.steemit.com/")
+		driver.get("https://www.steemit.com/")
 
-        
-        driver.implicitly_wait(7)                                                   #We find the login page and the text input.
-        usernameElem = driver.find_element_by_xpath(
-            "//*[@id='content']/div/div[2]/div/div/header/nav/div[3]/span[1]/a[1]"
-            ).send_keys(Keys.RETURN)
+		
+		driver.implicitly_wait(7)                                                   #We find the login page and the text input.
+		usernameElem = driver.find_element_by_xpath(
+			"//*[@id='content']/div/div[2]/div/div/header/nav/div[3]/span[1]/a[1]"
+			).send_keys(Keys.RETURN)
 
-        driver.implicitly_wait(7) #Wait to load the page                             #Username input.
-        usernameElem = driver.find_element_by_xpath(
-            "/html/body/div[2]/div/div[2]/div/div/form/div[1]/input"
-            ).send_keys(data['steemit']['username'])
-        
-        passwordElem = driver.find_element_by_xpath(                                 #Password input.
-            '/html/body/div[2]/div/div[2]/div/div/form/div[2]/input'
-            ).send_keys(data['steemit']['password'], Keys.RETURN)
+		driver.implicitly_wait(7) #Wait to load the page                             #Username input.
+		usernameElem = driver.find_element_by_xpath(
+			"/html/body/div[2]/div/div[2]/div/div/form/div[1]/input"
+			).send_keys(data['steemit']['username'])
+		
+		passwordElem = driver.find_element_by_xpath(                                 #Password input.
+			'/html/body/div[2]/div/div[2]/div/div/form/div[2]/input'
+			).send_keys(data['steemit']['password'], Keys.RETURN)
 
 
-        # Get write blog icon and fills each fields using the HTML file.
-        
-        wait = WebDriverWait(driver, 20)
+		# Get write blog icon and fills each fields using the HTML file.
+		
+		wait = WebDriverWait(driver, 20)
 
-                                                                                         
-        writePost = wait.until(EC.visibility_of_element_located((By.XPATH,             # wait for the button to be available
-            "/html/body/div/div/div[2]/div/div/header/nav/div[3]/a"
-            )))
+																						 
+		writePost = wait.until(EC.visibility_of_element_located((By.XPATH,             # wait for the button to be available
+			"/html/body/div/div/div[2]/div/div/header/nav/div[3]/a"
+			)))
 
-        writePost.click()                                                              #click the write blog button
-           
-        driver.implicitly_wait(5) 
+		writePost.click()                                                              #click the write blog button
+		   
+		driver.implicitly_wait(5) 
 
-        blogTags = driver.find_element_by_xpath(                                       #Fill in the blog post. This order is the right way so far to 
-            "/html/body/div/div/div[3]/div/div[2]/form/div[4]/span/span/input"         #input the post, otherwise, it all goes in the title for some reasons.
-            ).send_keys(tags)
-        blogTitle = wait.until(EC.visibility_of_element_located((By.XPATH, 
-            "/html/body/div/div/div[3]/div/div[2]/form/div[1]/span/input"
-            ))).send_keys(title)
-        blogBody = wait.until(EC.visibility_of_element_located((By.XPATH, 
-            "/html/body/div/div/div[3]/div/div[2]/form/div[2]/span/div/textarea"
-            ))).send_keys(body)
+		#Fill in the blog post. This order is the right way so far to 
+		#input the post, otherwise, it all goes in the title for some reasons.
+		
+		blogTitle = wait.until(EC.visibility_of_element_located((By.XPATH, 
+			"/html/body/div/div/div[3]/div/div[2]/form/div[1]/span/input"
+			))).send_keys(title + " - Blender 3D")
 
-       
-        return True
+		blogBody = wait.until(EC.visibility_of_element_located((By.XPATH, 
+			"/html/body/div/div/div[3]/div/div[2]/form/div[2]/span/div/textarea"
+			))).send_keys(body + "\nTimelapse of the whole thing below.\n\n\nNodes.\n\n" + steemitBody)
+		blogTags = driver.find_element_by_xpath(                                       
+			"/html/body/div/div/div[3]/div/div[2]/form/div[4]/span/span/input"         
+			  ).send_keys(tags)
+
+	   
+		return True
 
 steemit()
 
